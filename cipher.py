54b0d6db5a3b9bdd7404a256586e6a0e03af237f
Hamza Eloufraoui <heloufra@e2r11p12.1337.ma>
2022-07-03 02:57:50 +0100

    to upper

--- cipher.py
+++ cipher.py
@@ -1,24 +1,26 @@
 import random
 
 def generate_key():
-	key = [c for c in "abcdefghijklmnopqrstuvwxyz"]
+	key = [c for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 	random.shuffle(key)
 	return  "".join(key)
 
 def encrypt(plaintext, key):
 	ciphertext = ""
-	for c in plaintext.lower():
+	key = key.upper()
+	for c in plaintext.upper():
 		if c.isalpha():
-			ciphertext += key[ord(c) - ord('a')]
+			ciphertext += key[ord(c) - ord('A')]
 		else:
 			ciphertext += c
 	return ciphertext
 
 def decrypt(ciphertext, key):
 	plaintext = ""
-	for c in ciphertext:
+	key = key.upper()
+	for c in ciphertext.upper():
 		if c.isalpha():
-			plaintext += chr(key.index(c) + ord('a'))
+			plaintext += chr(key.index(c) + ord('A'))
 		else:
 			plaintext += c
 	return plaintext
